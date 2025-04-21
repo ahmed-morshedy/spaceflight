e019e39e2ff6b98a6d24b3008aba0a4c3fc56ef9
ahmed-morshedy <ahmedmorshedy270@yahoo.com>
2025-04-21 08:51:06 +0200

    fix useSearchParams error

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import MainPage from "./components/MainPage";
 import ArticleCard from "./ui/ArticleCard";
 
 export default function Home() {
   return (
     <div className="  min-h-screen pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <MainPage />
+      <Suspense>
+        <MainPage />
+      </Suspense>
     </div>
   );
 }
