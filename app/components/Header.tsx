6a0432565ad12ab00ab8b527d9387f0639f83e93
ahmed-morshedy <ahmedmorshedy270@yahoo.com>
2025-04-29 16:09:33 +0300

    update header style

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <header className="  w-[100%] h-[70px] bg-[#fff] z-[10] flex items-center justify-between pr-3 md:px-[20px] shadow-md  ">
+    <header className="  w-[100%] h-[70px] bg-[#fff] z-[10] flex items-center justify-between pr-1 md:pr-5 md:px-[20px] shadow-md  ">
       <a href="/">
         <p className=" text-xl md:text-3xl font-extrabold flex justify-center italic underline decoration-red-500   ">
           Spaceflight News
